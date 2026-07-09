05f79c7a43bdd1f303e4440568d84fb424e069c4
057gowtham-tech <057gowtham@gmail.com>
2026-07-09 21:00:38 +0530

    Update calculator.cpp

--- day1/calculator.cpp
+++ day1/calculator.cpp
@@ -1,1 +1,41 @@
-hi
+#include<iostream>
+  using namespace std;
+  int main()
+  {
+   double num1,num2;
+   char op;
+   cout<<"enter num1:";
+   cin>>num1;
+   cout<<"enter num2:";
+   cin>>num2;
+   cout<<"enter operator type:";
+   cin>>op;
+   
+   
+   if(op=='+')
+   {
+     cout<<num1+num2;
+   }
+   else if(op=='-')
+   {
+    cout<<num1-num2;
+   }
+   else if(op=='*')
+   {
+    cout<<num1*num2;
+   }
+   else if(op=='/')
+   {
+    cout<<num1/num2;
+   }
+   
+   else{
+    cout<<"op not found";
+    }
+   return 0;
+  } 
+    
+     
+   
+   
+   
